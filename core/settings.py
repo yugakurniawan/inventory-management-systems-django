d64a1536649563ac6e2b4fd941a6da6a10fe98d9
yugakurniawan <yugakurniawan@yahoo.com>
2020-12-23 14:54:11 +0700

    IMS

--- core/settings.py
+++ core/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ims-django',
+        'USER': 'root',
+        'PASSWORD':'',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
